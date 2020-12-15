aa82c8992202b52f162ede44aa26d5f71ca68fe6
Aimer <lovv840629y@gmail.com>
2020-12-15 21:49:00 +0800

    feat: 訂房詳情 、訂房成功，調整頁首頁尾

--- week8/app/assets/js/all.js
+++ week8/app/assets/js/all.js
@@ -1,2 +1,7 @@
 $(function() {
 });
+$(".js-reserveControl").click(function (e) {
+  e.preventDefault();
+  $(".js-reserveTotal").toggleClass("d-none");
+  $(".js-reserveIcon").toggleClass("rotate180");
+});
