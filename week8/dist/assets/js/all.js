4d24fc98bcebaa485df9689b13dd2524c359ffac
Aimer <lovv840629y@gmail.com>
2020-12-18 09:53:01 +0800

    chore: pust dist

---
+++ week8/dist/assets/js/all.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(function () {});
+$(".js-reserveControl").click(function (e) {
+  e.preventDefault();
+  $(".js-reserveTotal").toggleClass("d-none");
+  $(".js-reserveIcon").toggleClass("rotate180");
+});
+//# sourceMappingURL=all.js.map
