24a05f7df3c69e59c9695c9fa4265010d1480204
Aimer <lovv840629y@gmail.com>
2020-12-31 11:38:43 +0800

    chore: push dist

---
+++ week4/dist/assets/js/all.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log('Hello!');
+$(document).ready(function () {
+  console.log('HesSchool Hello!');
+  AOS.init({
+    duration: 2000
+  });
+});
+//# sourceMappingURL=all.js.map
